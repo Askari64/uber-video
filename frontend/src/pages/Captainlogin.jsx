9301cb0d619bc68f5f70c91c2e6d12da87465120
Askari <askarir64@gmail.com>
2025-04-11 16:30:56 +0530

    changing Uber occurences with ECO MOTO

--- frontend/src/pages/Captainlogin.jsx
+++ frontend/src/pages/Captainlogin.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { CaptainDataContext } from '../context/CapatainContext'
+import ECOMOTOLOGO from '../assets/ECOMOTO.png'
 
 const Captainlogin = () => {
 
